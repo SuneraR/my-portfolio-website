f04de34bd03bda03d7c7713444d387c91e7c0dac
SuneraR <114847925+SuneraR@users.noreply.github.com>
2025-12-11 23:26:55 +0530

    Fix netlify server action

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  output: 'export',
   reactCompiler: true,
   images: {
     unoptimized: true,
-  },
-  experimental: {
-    useDeploymentId: true,
-    useDeploymentIdServerActions: true,
   },
 };
 
